365e08e59954150ec5f7f753c088095eaa59e442
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-05-21 10:04:50 +0100

    amend relief investments models

---
+++ test/v1/models/requestData/amendReliefInvestments/AmendReliefInvestmentsBodySpec.scala
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.requestData.amendReliefInvestments
+
+import play.api.libs.json.Json
+import support.UnitSpec
+import v1.models.utils.JsonErrorValidators
+
+class AmendReliefInvestmentsBodySpec extends UnitSpec with JsonErrorValidators {
+  val amendReliefInvestmentsBody = AmendReliefInvestmentsBody(
+    Seq(VctSubscriptionsItem(
+      Some("VCTREF"),
+      Some("VCT Fund X"),
+      Some("2018-04-16"),
+      Some(BigDecimal(23312.00)),
+      Some(BigDecimal(1334.00))
+    )),
+    Seq(EisSubscriptionsItem(
+      Some("XTAL"),
+      Some("EIS Fund X"),
+      Some(true),
+      Some("2020-12-12"),
+      Some(BigDecimal(23312.00)),
+      Some(BigDecimal(43432.00))
+    )),
+    Seq(CommunityInvestmentItem(
+      Some("CIREF"),
+      Some("CI X"),
+      Some("2020-12-12"),
+      Some(BigDecimal(6442.00)),
+      Some(BigDecimal(2344.00))
+    )),
+    Seq(SeedEnterpriseInvestmentItem(
+      Some("123412/1A"),
+      Some("Company Inc"),
+      Some("2020-12-12"),
+      Some(BigDecimal(123123.22)),
+      Some(BigDecimal(3432.00))
+    )),
+    Seq(SocialEnterpriseInvestmentItem(
+      Some("123412/1A"),
+      Some("SE Inc"),
+      Some("2020-12-12"),
+      Some(BigDecimal(123123.22)),
+      Some(BigDecimal(3432.00))
+    ))
+  )
+  val json = Json.parse(
+    """
+      |{
+      |  "vctSubscription":[
+      |    {
+      |      "uniqueInvestmentRef": "VCTREF",
+      |      "name": "VCT Fund X",
+      |      "dateOfInvestment": "2018-04-16",
+      |      "amountInvested": 23312.00,
+      |      "reliefClaimed": 1334.00
+      |      }
+      |  ],
+      |  "eisSubscription":[
+      |    {
+      |      "uniqueInvestmentRef": "XTAL",
+      |      "name": "EIS Fund X",
+      |      "knowledgeIntensive": true,
+      |      "dateOfInvestment": "2020-12-12",
+      |      "amountInvested": 23312.00,
+      |      "reliefClaimed": 43432.00
+      |    }
+      |  ],
+      |  "communityInvestment": [
+      |    {
+      |      "uniqueInvestmentRef": "CIREF",
+      |      "name": "CI X",
+      |      "dateOfInvestment": "2020-12-12",
+      |      "amountInvested": 6442.00,
+      |      "reliefClaimed": 2344.00
+      |    }
+      |  ],
+      |  "seedEnterpriseInvestment": [
+      |    {
+      |      "uniqueInvestmentRef": "123412/1A",
+      |      "companyName": "Company Inc",
+      |      "dateOfInvestment": "2020-12-12",
+      |      "amountInvested": 123123.22,
+      |      "reliefClaimed": 3432.00
+      |    }
+      |  ],
+      |  "socialEnterpriseInvestment": [
+      |    {
+      |      "uniqueInvestmentRef": "123412/1A",
+      |      "socialEnterpriseName": "SE Inc",
+      |      "dateOfInvestment": "2020-12-12",
+      |      "amountInvested": 123123.22,
+      |      "reliefClaimed": 3432.00
+      |    }
+      |  ]
+      |}
+        """.stripMargin
+  )
+
+
+  "reads" when {
+    "passed valid JSON" should {
+      "return a valid model" in {
+        amendReliefInvestmentsBody shouldBe json.as[AmendReliefInvestmentsBody]
+      }
+    }
+  }
+  "writes" when {
+    "passed valid model" should {
+      "return valid json" in {
+        Json.toJson(amendReliefInvestmentsBody) shouldBe json
+      }
+    }
+  }
+}
---
+++ test/v1/models/requestData/amendReliefInvestments/SeedEnterpriseInvestmentItemSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.requestData.amendReliefInvestments
+
+import play.api.libs.json.Json
+import support.UnitSpec
+import v1.models.utils.JsonErrorValidators
+
+class SeedEnterpriseInvestmentItemSpec extends UnitSpec with JsonErrorValidators {
+  val seedEnterpriseInvestmentItem = SeedEnterpriseInvestmentItem(
+    Some("123412/1A"),
+    Some("Company Inc"),
+    Some("2020-12-12"),
+    Some(BigDecimal(123123.22)),
+    Some(BigDecimal(3432.00))
+  )
+  val json = Json.parse(
+    """
+      |{
+      |  "uniqueInvestmentRef": "123412/1A",
+      |  "companyName": "Company Inc",
+      |  "dateOfInvestment": "2020-12-12",
+      |  "amountInvested": 123123.22,
+      |  "reliefClaimed": 3432.00
+      |}
+        """.stripMargin
+  )
+
+
+  "reads" when {
+    "passed valid JSON" should {
+      "return a valid model" in {
+        seedEnterpriseInvestmentItem shouldBe json.as[SeedEnterpriseInvestmentItem]
+      }
+    }
+  }
+  "writes" when {
+    "passed valid model" should {
+      "return valid json" in {
+        Json.toJson(seedEnterpriseInvestmentItem) shouldBe json
+      }
+    }
+  }
+}
