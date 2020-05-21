e3542a98cfc89fa2942f9fd2e7866817310107e4
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2020-05-21 15:18:02 +0100

    amend relief investments models (#5)

---
+++ test/v1/models/requestData/amendReliefInvestments/CommunityInvestmentItemSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.requestData.amendReliefInvestments
+
+import play.api.libs.json.Json
+import support.UnitSpec
+import v1.models.utils.JsonErrorValidators
+
+class CommunityInvestmentItemSpec extends UnitSpec with JsonErrorValidators {
+  val communityInvestmentItem = CommunityInvestmentItem(
+    Some("VCTREF"),
+    Some("VCT Fund X"),
+    Some("2018-04-16"),
+    Some(BigDecimal(23312.00)),
+    Some(BigDecimal(1334.00))
+  )
+  val json = Json.parse(
+    """
+      |{
+      |  "uniqueInvestmentRef": "VCTREF",
+      |  "name": "VCT Fund X",
+      |  "dateOfInvestment": "2018-04-16",
+      |  "amountInvested": 23312.00,
+      |  "reliefClaimed": 1334.00
+      |}
+        """.stripMargin
+  )
+
+
+  "reads" when {
+    "passed valid JSON" should {
+      "return a valid model" in {
+        communityInvestmentItem shouldBe json.as[CommunityInvestmentItem]
+      }
+    }
+  }
+  "writes" when {
+    "passed valid model" should {
+      "return valid json" in {
+        Json.toJson(communityInvestmentItem) shouldBe json
+      }
+    }
+  }
+}
---
+++ test/v1/models/requestData/amendReliefInvestments/EisSubscriptionsItemSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.requestData.amendReliefInvestments
+
+import play.api.libs.json.Json
+import support.UnitSpec
+import v1.models.utils.JsonErrorValidators
+
+class EisSubscriptionsItemSpec extends UnitSpec with JsonErrorValidators {
+  val eisSubscriptionsItem = EisSubscriptionsItem(
+    Some("XTAL"),
+    Some("EIS Fund X"),
+    Some(true),
+    Some("EIS Fund X"),
+    Some(BigDecimal(23312.00)),
+    Some(BigDecimal(43432.00))
+  )
+  val json = Json.parse(
+    """
+      |{
+      |  "uniqueInvestmentRef": "XTAL",
+      |  "name": "EIS Fund X",
+      |  "knowledgeIntensive": true,
+      |  "dateOfInvestment": "EIS Fund X",
+      |  "amountInvested": 23312.00,
+      |  "reliefClaimed": 43432.00
+      |}
+        """.stripMargin
+  )
+
+
+  "reads" when {
+    "passed valid JSON" should {
+      "return a valid model" in {
+        eisSubscriptionsItem shouldBe json.as[EisSubscriptionsItem]
+      }
+    }
+  }
+  "writes" when {
+    "passed valid model" should {
+      "return valid json" in {
+        Json.toJson(eisSubscriptionsItem) shouldBe json
+      }
+    }
+  }
+}
