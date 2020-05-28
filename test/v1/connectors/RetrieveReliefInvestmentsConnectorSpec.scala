8a48636baf33d0ca1cd9e0604cb699f9d50dba39
HarryBaxter <harry.baxter@digital.hmrc.gov.uk>
2020-05-28 15:41:16 +0100

    MTDSA-5553 Connector and Service for Retrieve Relief Investments

---
+++ test/v1/connectors/RetrieveReliefInvestmentsConnectorSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import mocks.MockAppConfig
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.MockHttpClient
+import v1.models.outcomes.ResponseWrapper
+import v1.models.requestData.retrieveReliefInvestments.RetrieveReliefInvestmentsRequest
+import v1.models.response.retrieveReliefInvestments.RetrieveReliefInvestmentsBody
+
+import scala.concurrent.Future
+
+class RetrieveReliefInvestmentsConnectorSpec extends ConnectorSpec {
+
+  val taxYear = "2017-18"
+  val nino = Nino("AA123456A")
+
+  class Test extends MockHttpClient with MockAppConfig {
+    val connector: RetrieveReliefInvestmentsConnector = new RetrieveReliefInvestmentsConnector(http = mockHttpClient, appConfig = mockAppConfig)
+
+    val desRequestHeaders: Seq[(String, String)] = Seq("Environment" -> "des-environment", "Authorization" -> s"Bearer des-token")
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "RetrieveReliefInvestmentsConnector" when {
+    "retrieving relief investments" must {
+      val request: RetrieveReliefInvestmentsRequest = RetrieveReliefInvestmentsRequest(nino, taxYear)
+
+      "return a valid response" in new Test {
+        val outcome = Right(ResponseWrapper(correlationId, RetrieveReliefInvestmentsBody))
+
+        MockedHttpClient
+          .get(
+            url = s"$baseUrl/reliefs/investment/$nino/$taxYear",
+            requiredHeaders = "Environment" -> "des-environment", "Authorization" -> s"Bearer des-token"
+          )
+          .returns(Future.successful(outcome))
+
+      await(connector.retrieveReliefInvestments(request)) shouldBe outcome
+    }
+  }
+}
