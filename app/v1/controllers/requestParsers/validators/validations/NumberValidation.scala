a881a8b4b25f0a01840638b8f0274d461599ebaa
“DreamcastKnight” <56401347+DreamcastKnight@users.noreply.github.com>
2020-06-04 10:49:55 +0100

    changed validate in number validation to private

--- app/v1/controllers/requestParsers/validators/validations/NumberValidation.scala
+++ app/v1/controllers/requestParsers/validators/validations/NumberValidation.scala
@@ -28,7 +28,7 @@ object NumberValidation {
   }
 
 
-  def validate(field: BigDecimal, path: String): List[MtdError] = {
+  private def validate(field: BigDecimal, path: String): List[MtdError] = {
     if (field >= 1 && field < 100000000000.00 && field.scale <= 2) {
       Nil
     } else {
