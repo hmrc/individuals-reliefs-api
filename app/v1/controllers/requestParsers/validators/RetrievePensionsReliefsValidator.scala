a8045e659f9e3b727d11fad4cb24ae739836fcc7
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2020-07-24 10:26:54 +0100

    [MTDSA-6462] - retrieve pensions reliefs parser & validator (#94)
    * retrieve parser and validator
    
    * finished tests
    
    * removed unused import
    
    * added tax year not supported error to tests and validator

---
+++ app/v1/controllers/requestParsers/validators/RetrievePensionsReliefsValidator.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import v1.controllers.requestParsers.validators.validations.{MtdTaxYearValidation, NinoValidation, TaxYearValidation}
+import v1.models.errors.{MtdError, RuleTaxYearNotSupportedError}
+import v1.models.request.retrievePensionsReliefs.RetrievePensionsReliefsRawData
+
+class RetrievePensionsReliefsValidator extends Validator[RetrievePensionsReliefsRawData] {
+
+
+  private val validationSet = List(parameterFormatValidation, parameterRuleValidation)
+
+  private def parameterFormatValidation: RetrievePensionsReliefsRawData => List[List[MtdError]] = data => {
+    List(
+      NinoValidation.validate(data.nino),
+      TaxYearValidation.validate(data.taxYear)
+    )
+  }
+
+  private def parameterRuleValidation: RetrievePensionsReliefsRawData => List[List[MtdError]] = (data: RetrievePensionsReliefsRawData) => {
+    List(
+      MtdTaxYearValidation.validate(data.taxYear, RuleTaxYearNotSupportedError)
+    )
+  }
+
+  override def validate(data: RetrievePensionsReliefsRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+}
