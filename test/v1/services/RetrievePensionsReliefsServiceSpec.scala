bce072604eb95f43094acda77a40fa8bc3577dec
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-07-27 09:59:58 +0100

    MTDSA-6463 Retrieve Pensions Reliefs Connector and Service (#95)
    * MTDSA-6463 Retrieve Pensions Reliefs Connector and Service
    
    * MTDSA-6463 Renaming / import organisation

---
+++ test/v1/services/RetrievePensionsReliefsServiceSpec.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.controllers.EndpointLogContext
+import v1.mocks.connectors.MockRetrievePensionsReliefsConnector
+import v1.models.errors.{DesErrorCode, DesErrors, DownstreamError, ErrorWrapper, MtdError, NinoFormatError, NotFoundError, TaxYearFormatError}
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.retrievePensionsReliefs.RetrievePensionsReliefsRequest
+import v1.models.response.retrievePensionsReliefs.{PensionsReliefs, RetrievePensionsReliefsResponse}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class RetrievePensionsReliefsServiceSpec extends UnitSpec {
+
+  private val nino = "AA123456A"
+  private val taxYear = "2017-18"
+  private val correlationId = "X-123"
+
+  private val fullResponseModel = RetrievePensionsReliefsResponse(
+    "2019-04-04T01:01:01Z",
+    PensionsReliefs(
+      Some(1999.99),
+      Some(1999.99),
+      Some(1999.99),
+      Some(1999.99),
+      Some(1999.99)
+    )
+  )
+
+  private val requestData = RetrievePensionsReliefsRequest(Nino(nino), taxYear)
+
+  trait Test extends MockRetrievePensionsReliefsConnector {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+    val service = new RetrievePensionsReliefsService(
+      connector = mockConnector
+    )
+  }
+
+  "service" when {
+    "service call successful" must {
+      "return mapped result" in new Test {
+        MockRetrievePensionsReliefsConnector.retrieve(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, fullResponseModel))))
+
+        await(service.retrieve(requestData)) shouldBe Right(ResponseWrapper(correlationId, fullResponseModel))
+      }
+    }
+
+    "unsuccessful" must {
+      "map errors according to spec" when {
+
+        def serviceError(desErrorCode: String, error: MtdError): Unit =
+          s"a $desErrorCode error is returned from the service" in new Test {
+
+            MockRetrievePensionsReliefsConnector.retrieve(requestData)
+              .returns(Future.successful(Left(ResponseWrapper(correlationId, DesErrors.single(DesErrorCode(desErrorCode))))))
+
+            await(service.retrieve(requestData)) shouldBe Left(ErrorWrapper(Some(correlationId), error))
+          }
+
+        val input = Seq(
+          ("INVALID_TAXABLE_ENTITY_ID", NinoFormatError),
+          ("INVALID_TAX_YEAR_EXPLICIT", TaxYearFormatError),
+          ("NOT_FOUND", NotFoundError),
+          ("SERVER_ERROR", DownstreamError),
+          ("SERVICE_UNAVAILABLE", DownstreamError)
+        )
+
+        input.foreach(args => (serviceError _).tupled(args))
+      }
+    }
+  }
+}
