ddfafe1aebd15f8e05a1ad41298559f86843f0db
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-07-30 09:29:27 +0100

    [MTDSA-6550] Add tax year not supported error to Delete Other Reliefs (#102)
    * MTDSA-6550 Add tax year not supported error to Delete Other Reliefs
    
    * Resolved comments

--- it/v1/endpoints/DeleteOtherReliefsControllerISpec.scala
+++ it/v1/endpoints/DeleteOtherReliefsControllerISpec.scala
@@ -94,9 +94,10 @@ class DeleteOtherReliefsControllerISpec extends IntegrationBaseSpec {
         }
 
         val input = Seq(
-          ("Walrus", "2019-20", Status.BAD_REQUEST, NinoFormatError),
+          ("Walrus", "2021-22", Status.BAD_REQUEST, NinoFormatError),
           ("AA123456A", "203100", Status.BAD_REQUEST, TaxYearFormatError),
-          ("AA123456A", "2018-20", Status.BAD_REQUEST, RuleTaxYearRangeInvalidError)
+          ("AA123456A", "2018-20", Status.BAD_REQUEST, RuleTaxYearRangeInvalidError),
+          ("AA123456A", "2020-21", Status.BAD_REQUEST, RuleTaxYearNotSupportedError)
         )
 
 
