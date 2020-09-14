a1b70b5a1abece3397600d163eaf146ed95f7d96
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2020-09-14 14:02:26 +0100

    begun audits for other reliefs

---
+++ app/v1/models/audit/DeleteOtherReliefsAuditDetail.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.audit
+
+import play.api.libs.json.{Json, Writes}
+import v1.models.auth.UserDetails
+
+case class DeleteOtherReliefsAuditDetail(userType: String,
+                                         agentReferenceNumber: Option[String],
+                                         nino: String,
+                                         taxYear: String,
+                                         `X-CorrelationId`: String,
+                                         response: AuditResponse)
+
+object DeleteOtherReliefsAuditDetail {
+  implicit val writes: Writes[DeleteOtherReliefsAuditDetail] = Json.writes[DeleteOtherReliefsAuditDetail]
+
+  def apply(userDetails: UserDetails,
+            nino: String,
+            taxYear: String,
+            `X-CorrelationId`: String,
+            auditResponse: AuditResponse): DeleteOtherReliefsAuditDetail = {
+
+    DeleteOtherReliefsAuditDetail(
+      userType = userDetails.userType,
+      agentReferenceNumber = userDetails.agentReferenceNumber,
+      nino = nino,
+      taxYear = taxYear,
+      `X-CorrelationId`,
+      auditResponse)
+  }
+}
+
