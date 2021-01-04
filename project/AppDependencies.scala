c4e23d162f9c37207059d91557ccf1ea953b2f10
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2021-01-04 10:43:15 +0000

    update dependencies and headers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,8 +22,9 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"   %% "bootstrap-play-26" % "1.5.0",
-    "uk.gov.hmrc"   %% "domain"            % "5.9.0-play-26",
+
+    "uk.gov.hmrc"   %% "bootstrap-play-26" % "2.3.0",
+    "uk.gov.hmrc"   %% "domain"            % "5.10.0-play-26",
     "uk.gov.hmrc"   %% "play-hmrc-api"     % "4.1.0-play-26",
     "org.typelevel" %% "cats-core"         % "2.2.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.6.0" cross CrossVersion.full),
