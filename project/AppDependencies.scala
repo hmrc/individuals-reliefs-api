1c8165e9893f14e79547b73521e1f2c14f0f4f4b
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2021-06-01 11:26:28 +0100

    [MTDSA-9074] Migrate silencer settings/dependencies to separate file. Removed wiremock-jre overrides.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -27,8 +27,6 @@ object AppDependencies {
     "uk.gov.hmrc"        %% "domain"                    % "5.10.0-play-26",
     "uk.gov.hmrc"        %% "play-hmrc-api"             % "4.1.0-play-26",
     "org.typelevel"      %% "cats-core"                 % "2.3.0",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.0" cross CrossVersion.full),
-    "com.github.ghik"    % "silencer-lib" % "1.7.0" % Provided cross CrossVersion.full,
     "com.chuusai"        %% "shapeless"                 % "2.4.0-M1",
     "com.neovisionaries" % "nv-i18n"                    % "1.27"
   )
@@ -42,25 +40,4 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3"             % scope,
     "com.github.tomakehurst" % "wiremock"            % "2.27.2"            % scope
   )
-
-  // Fixes a transitive dependency clash between wiremock and scalatestplus-play
-  val overrides: Seq[ModuleID] = {
-    val jettyFromWiremockVersion = "9.2.24.v20180105"
-    Seq(
-      "org.eclipse.jetty"           % "jetty-client"       % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-continuation" % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-http"         % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-io"           % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-security"     % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-server"       % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-servlet"      % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-servlets"     % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-util"         % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-webapp"       % jettyFromWiremockVersion,
-      "org.eclipse.jetty"           % "jetty-xml"          % jettyFromWiremockVersion,
-      "org.eclipse.jetty.websocket" % "websocket-api"      % jettyFromWiremockVersion,
-      "org.eclipse.jetty.websocket" % "websocket-client"   % jettyFromWiremockVersion,
-      "org.eclipse.jetty.websocket" % "websocket-common"   % jettyFromWiremockVersion
-    )
-  }
 }
