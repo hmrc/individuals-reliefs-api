5b2890575ffc5305317e1175521bf1a7ae6a540c
RachGibbons <rach.gibbons@digital.hmrc.gov.uk>
2021-10-08 08:50:24 +0100

    MTDSA-10136 Changed error message for code

--- app/v1/services/AmendOtherReliefsService.scala
+++ app/v1/services/AmendOtherReliefsService.scala
@@ -50,7 +50,7 @@ class AmendOtherReliefsService @Inject()(connector: AmendOtherReliefsConnector) 
       "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
       "FORMAT_TAX_YEAR" -> TaxYearFormatError,
       "INVALID_CORRELATIONID"  -> DownstreamError,
-      "BUSINESS_VALIDATION" -> RuleSubmissionFailed,
+      "BUSINESS_VALIDATION_RULE_FAILURE" -> RuleSubmissionFailed,
       "SERVER_ERROR" -> DownstreamError,
       "SERVICE_UNAVAILABLE" -> DownstreamError
     )
