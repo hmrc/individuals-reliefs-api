cb15b26eaa74ecd0be93839b286db5b3b91a2ede
Sean Flynn <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-06-07 14:36:31 +0100

    Trying to rebase 2

--- app/v1/controllers/DeleteCharitableGivingController.scala
+++ app/v1/controllers/DeleteCharitableGivingController.scala
@@ -1,5 +1,110 @@
-package v1.controllers.requestParsers
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-class DeleteCharitableGivingController {
+package v1.controllers
+
+import cats.data.EitherT
+import cats.implicits._
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.{IdGenerator, Logging}
+import v1.controllers.requestParsers.DeleteForeignReliefsRequestParser
+import v1.models.audit.{AuditEvent, AuditResponse, DeleteForeignReliefsAuditDetail}
+import v1.models.errors._
+import v1.models.request.deleteForeignReliefs.DeleteForeignReliefsRawData
+import v1.services.{AuditService, DeleteForeignReliefsService, EnrolmentsAuthService, MtdIdLookupService}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DeleteCharitableGivingController @Inject() (val authService: EnrolmentsAuthService,
+                                                val lookupService: MtdIdLookupService,
+                                                parser: DeleteCharitableGivingRequestParser,
+                                                service: DeleteCharitableGivingService,
+                                                auditService: AuditService,
+                                                cc: ControllerComponents,
+                                                val idGenerator: IdGenerator)(implicit ec: ExecutionContext)
+  extends AuthorisedController(cc)
+    with BaseController
+    with Logging {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(controllerName = "DeleteCharitableGivingController", endpointName = "deleteCharitableGiving")
+
+  def handleRequest(nino: String, taxYear: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      implicit val correlationId: String = idGenerator.getCorrelationId
+      logger.info(message = s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+        s"with correlationId : $correlationId")
+      val rawData = DeleteCharitableGivingRawData(nino, taxYear)
+      val result =
+        for {
+          parsedRequest   <- EitherT.fromEither[Future](parser.parseRequest(rawData))
+          serviceResponse <- EitherT(service.delete(parsedRequest))
+        } yield {
+          logger.info(
+            s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+              s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+          auditSubmission(
+            DeleteForeignReliefsAuditDetail(
+              request.userDetails,
+              nino,
+              taxYear,
+              serviceResponse.correlationId,
+              AuditResponse(NO_CONTENT, Right(None))))
+
+          NoContent.withApiHeaders(serviceResponse.correlationId)
+
+        }
+      result.leftMap { errorWrapper =>
+        val resCorrelationId = errorWrapper.correlationId
+        val result           = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+
+        logger.warn(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Error response received with CorrelationId: $resCorrelationId")
+
+        auditSubmission(
+          DeleteForeignReliefsAuditDetail(
+            request.userDetails,
+            nino,
+            taxYear,
+            correlationId,
+            AuditResponse(result.header.status, Left(errorWrapper.auditErrors))))
+
+        result
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) = {
+    errorWrapper.error match {
+      case NinoFormatError | BadRequestError | TaxYearFormatError | RuleTaxYearNotSupportedError | RuleTaxYearRangeInvalidError =>
+        BadRequest(Json.toJson(errorWrapper))
+      case DownstreamError => InternalServerError(Json.toJson(errorWrapper))
+      case NotFoundError   => NotFound(Json.toJson(errorWrapper))
+      case _               => unhandledError(errorWrapper)
+    }
+  }
+
+  private def auditSubmission(details: DeleteForeignReliefsAuditDetail)(implicit hc: HeaderCarrier, ec: ExecutionContext) = {
+    val event = AuditEvent("DeleteForeignReliefs", "delete-foreign-reliefs", details)
+    auditService.auditEvent(event)
+  }
 
 }
