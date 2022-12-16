5708754507735047f3171fbf3ac5f4eb3b2deda2
tom-lloyd-hmrc <112491110+tom-lloyd-hmrc@users.noreply.github.com>
2022-12-16 12:02:00 +0000

    MTDSA-14152: TYS Updates For Create And Amend Relief Investments

--- test/v1/controllers/requestParsers/CreateAndAmendReliefInvestmentsRequestParserSpec.scala
+++ test/v1/controllers/requestParsers/CreateAndAmendReliefInvestmentsRequestParserSpec.scala
@@ -16,69 +16,18 @@
 
 package v1.controllers.requestParsers
 
-import play.api.libs.json.Json
 import support.UnitSpec
-import v1.models.domain.Nino
+import v1.fixtures.CreateAndAmendReliefInvestmentsFixtures.{requestBodyJson, requestBodyModel}
 import v1.mocks.validators.MockCreateAndAmendReliefInvestmentValidator
+import v1.models.domain.Nino
 import v1.models.errors.{BadRequestError, ErrorWrapper, NinoFormatError, TaxYearFormatError}
 import v1.models.request.TaxYear
 import v1.models.request.createAndAmendReliefInvestments._
 
-class AmendReliefInvestmentDataParserSpec extends UnitSpec {
+class CreateAndAmendReliefInvestmentsRequestParserSpec extends UnitSpec {
   private val nino: String           = "AA123456A"
   private val taxYear: String        = "2021-22"
   implicit val correlationId: String = "a1e8057e-fbbc-47a8-a8b4-78d9f015c253"
-
-  private val requestBodyJson = Json.parse("""
-      |{
-      |  "vctSubscription":[
-      |    {
-      |      "uniqueInvestmentRef": "VCTREF",
-      |      "name": "VCT Fund X",
-      |      "dateOfInvestment": "2018-04-16",
-      |      "amountInvested": 23312.00,
-      |      "reliefClaimed": 1334.00
-      |      }
-      |  ],
-      |  "eisSubscription":[
-      |    {
-      |      "uniqueInvestmentRef": "XTAL",
-      |      "name": "EIS Fund X",
-      |      "knowledgeIntensive": true,
-      |      "dateOfInvestment": "2020-12-12",
-      |      "amountInvested": 23312.00,
-      |      "reliefClaimed": 43432.00
-      |    }
-      |  ],
-      |  "communityInvestment": [
-      |    {
-      |      "uniqueInvestmentRef": "CIREF",
-      |      "name": "CI X",
-      |      "dateOfInvestment": "2020-12-12",
-      |      "amountInvested": 6442.00,
-      |      "reliefClaimed": 2344.00
-      |    }
-      |  ],
-      |  "seedEnterpriseInvestment": [
-      |    {
-      |      "uniqueInvestmentRef": "123412/1A",
-      |      "companyName": "Company Inc",
-      |      "dateOfInvestment": "2020-12-12",
-      |      "amountInvested": 123123.22,
-      |      "reliefClaimed": 3432.00
-      |    }
-      |  ],
-      |  "socialEnterpriseInvestment": [
-      |    {
-      |      "uniqueInvestmentRef": "123412/1A",
-      |      "socialEnterpriseName": "SE Inc",
-      |      "dateOfInvestment": "2020-12-12",
-      |      "amountInvested": 123123.22,
-      |      "reliefClaimed": 3432.00
-      |    }
-      |  ]
-      |}
-        """.stripMargin)
 
   val inputData: CreateAndAmendReliefInvestmentsRawData =
     CreateAndAmendReliefInvestmentsRawData(nino, taxYear, requestBodyJson)
@@ -98,13 +47,7 @@ class AmendReliefInvestmentDataParserSpec extends UnitSpec {
             CreateAndAmendReliefInvestmentsRequest(
               Nino(nino),
               TaxYear.fromMtd(taxYear),
-              CreateAndAmendReliefInvestmentsBody(
-                Some(Seq(VctSubscriptionsItem("VCTREF", Some("VCT Fund X"), Some("2018-04-16"), Some(23312.00), 1334.00))),
-                Some(Seq(EisSubscriptionsItem("XTAL", Some("EIS Fund X"), true, Some("2020-12-12"), Some(23312.00), 43432.00))),
-                Some(Seq(CommunityInvestmentItem("CIREF", Some("CI X"), Some("2020-12-12"), Some(6442.00), 2344.00))),
-                Some(Seq(SeedEnterpriseInvestmentItem("123412/1A", Some("Company Inc"), Some("2020-12-12"), Some(123123.22), 3432.00))),
-                Some(Seq(SocialEnterpriseInvestmentItem("123412/1A", Some("SE Inc"), Some("2020-12-12"), Some(123123.22), 3432.00)))
-              )
+              requestBodyModel
             ))
       }
     }
