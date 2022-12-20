a64f5795c5b78617e0e4d501534171507a3ef8cf
tom-lloyd-hmrc <112491110+tom-lloyd-hmrc@users.noreply.github.com>
2022-12-20 14:52:21 +0000

    MTDSA-14168: Renamed Endpoint To Include Create

---
+++ app/v1/services/CreateAndAmendForeignReliefsService.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import cats.data.EitherT
+import cats.implicits._
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.CreateAndAmendForeignReliefsConnector
+import v1.controllers.EndpointLogContext
+import v1.models.errors._
+import v1.models.request.createAndAmendForeignReliefs.CreateAndAmendForeignReliefsRequest
+import v1.support.DownstreamResponseMappingSupport
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class CreateAndAmendForeignReliefsService @Inject() (connector: CreateAndAmendForeignReliefsConnector)
+    extends DownstreamResponseMappingSupport
+    with Logging {
+
+  def createAndAmend(request: CreateAndAmendForeignReliefsRequest)(implicit
+                                                                   hc: HeaderCarrier,
+                                                                   ec: ExecutionContext,
+                                                                   logContext: EndpointLogContext,
+                                                                   correlationId: String): Future[ServiceOutcome[Unit]] = {
+
+    val result = for {
+      responseWrapper <- EitherT(connector.createAndAmend(request)).leftMap(mapDownstreamErrors(desErrorMap))
+    } yield responseWrapper
+
+    result.value
+  }
+
+  private def desErrorMap =
+    Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "FORMAT_TAX_YEAR"           -> TaxYearFormatError,
+      "SERVER_ERROR"              -> InternalError,
+      "SERVICE_UNAVAILABLE"       -> InternalError
+    )
+
+}
---
+++ test/v1/services/CreateAndAmendForeignReliefsServiceSpec.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import support.UnitSpec
+import v1.models.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.controllers.EndpointLogContext
+import v1.mocks.connectors.MockCreateAndAmendForeignReliefsConnector
+import v1.models.errors._
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.TaxYear
+import v1.models.request.createAndAmendForeignReliefs._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class CreateAndAmendForeignReliefsServiceSpec extends UnitSpec {
+
+  private val taxYear                        = "2017-18"
+  private val nino                           = Nino("AA123456A")
+  private implicit val correlationId: String = "X-123"
+  private val amount: BigDecimal             = 1234.56
+
+  private val body = CreateAndAmendForeignReliefsBody(
+    foreignTaxCreditRelief = Some(
+      ForeignTaxCreditRelief(
+        amount = amount
+      )),
+    foreignIncomeTaxCreditRelief = Some(
+      Seq(
+        ForeignIncomeTaxCreditRelief(
+          countryCode = "FRA",
+          foreignTaxPaid = Some(amount),
+          taxableAmount = amount,
+          employmentLumpSum = true
+        ))),
+    foreignTaxForFtcrNotClaimed = Some(
+      ForeignTaxForFtcrNotClaimed(
+        amount = amount
+      ))
+  )
+
+  private val requestData = CreateAndAmendForeignReliefsRequest(nino, TaxYear.fromMtd(taxYear), body)
+
+  trait Test extends MockCreateAndAmendForeignReliefsConnector {
+    implicit val hc: HeaderCarrier              = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new CreateAndAmendForeignReliefsService(
+      connector = mockCreateAndAmendForeignReliefsConnector
+    )
+
+  }
+
+  "service" when {
+    "service call successful" must {
+      "return mapped result" in new Test {
+        MockCreateAndAmendForeignReliefsConnector
+          .createAndAmend(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        await(service.createAndAmend(requestData)) shouldBe Right(ResponseWrapper(correlationId, ()))
+      }
+    }
+  }
+
+  "unsuccessful" must {
+    "map errors according to spec" when {
+
+      def serviceError(desErrorCode: String, error: MtdError): Unit =
+        s"a $desErrorCode error is returned from the service" in new Test {
+
+          MockCreateAndAmendForeignReliefsConnector
+            .createAndAmend(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(desErrorCode))))))
+
+          await(service.createAndAmend(requestData)) shouldBe Left(ErrorWrapper(correlationId, error))
+        }
+
+      val input = Seq(
+        "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+        "FORMAT_TAX_YEAR"           -> TaxYearFormatError,
+        "SERVER_ERROR"              -> InternalError,
+        "SERVICE_UNAVAILABLE"       -> InternalError
+      )
+
+      input.foreach(args => (serviceError _).tupled(args))
+    }
+  }
+
+}
