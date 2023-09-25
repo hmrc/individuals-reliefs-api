0a0871dfd6c6aad382a27f0117390c0e394d76ce
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-09-25 13:36:24 +0100

    MTDSA-19395: Added Scala doc comments to ValidatorOps

--- app/api/controllers/validators/ValidatorOps.scala
+++ app/api/controllers/validators/ValidatorOps.scala
@@ -23,6 +23,19 @@ import cats.implicits.toTraverseOps
 
 trait ValidatorOps {
 
+  /** Validates fields with their associated paths.
+    *
+    * @param fieldsWithPaths
+    *   A sequence of fields with their associated paths.
+    * @param validate
+    *   A function to validate each field with its path.
+    * @tparam A
+    *   The type of the field.
+    * @tparam B
+    *   The type of the validation result.
+    * @return
+    *   A validation result.
+    */
   protected def validateWithPaths[A, B](fieldsWithPaths: (Option[A], String)*)(
       validate: (A, Option[String]) => Validated[Seq[MtdError], B]): Validated[Seq[MtdError], Unit] =
     fieldsWithPaths
@@ -33,28 +46,92 @@ trait ValidatorOps {
       .sequence
       .andThen(_ => Valid(()))
 
+  /** Provides utility operations for validating optional fields.
+    *
+    * @param maybeFields
+    *   An optional field.
+    * @tparam A
+    *   The type of the field.
+    */
   implicit class ValidatorOptionOps[A](maybeFields: Option[A]) {
 
+    /** Maps the field to a validation result or returns a default valid value.
+      *
+      * @param mapping
+      *   A function to map the field to a validation result.
+      * @param defaultValid
+      *   A default validation result.
+      * @tparam B
+      *   The type of the validation result.
+      * @return
+      *   A validation result.
+      */
     def mapOrElse[B](mapping: A => Validated[Seq[MtdError], B], defaultValid: Valid[B]): Validated[Seq[MtdError], B] =
       maybeFields.map(mapping).getOrElse(defaultValid)
 
+    /** Maps the field to a validation result or returns a default valid value of type Valid[Unit].
+      *
+      * @param mapping
+      *   A function to map the field to a validation result.
+      * @return
+      *   A validation result.
+      */
     def mapOrElse(mapping: A => Validated[Seq[MtdError], Unit]): Validated[Seq[MtdError], Unit] =
       mapOrElse(mapping, Valid(()))
 
   }
 
+  /** Provides utility operations for validating sequences of fields.
+    *
+    * @param fields
+    *   A sequence of fields.
+    * @tparam A
+    *   The type of the field.
+    */
   implicit class ValidatorSeqOps[A](fields: Seq[A]) {
 
+    /** Validates each field in the sequence with its index and returns a validation result.
+      *
+      * @param validate
+      *   A function to validate each field with its index.
+      * @return
+      *   A validation result.
+      */
     def zipAndValidate(validate: (A, Int) => Validated[Seq[MtdError], Unit]): Validated[Seq[MtdError], Unit] =
       fields.zipWithIndex.traverse(validate.tupled).map(_ => ())
 
   }
 
+  /** Provides utility operations for validating optional sequences of fields.
+    *
+    * @param maybeFields
+    *   An optional sequence of fields.
+    * @tparam A
+    *   The type of the field.
+    */
   implicit class ValidatorOptionSeqOps[A](maybeFields: Option[Seq[A]]) {
 
+    /** Validates each field in the optional sequence with its index or returns a default value.
+      *
+      * @param validate
+      *   A function to validate each field with its index.
+      * @param defaultValid
+      *   A default validation result.
+      * @tparam B
+      *   The type of the validation result.
+      * @return
+      *   A validation result.
+      */
     def zipAndValidate[B](validate: (A, Int) => Validated[Seq[MtdError], B], defaultValid: Valid[Seq[B]]): Validated[Seq[MtdError], Seq[B]] =
       maybeFields.mapOrElse((fields: Seq[A]) => fields.zipWithIndex.traverse(validate.tupled), defaultValid)
 
+    /** Validates each field in the optional sequence with its index or returns a default value of Valid[Unit].
+      *
+      * @param validate
+      *   A function to validate each field with its index.
+      * @return
+      *   A validation result.
+      */
     def zipAndValidate(validate: (A, Int) => Validated[Seq[MtdError], Unit]): Validated[Seq[MtdError], Unit] =
       maybeFields.mapOrElse((fields: Seq[A]) => fields.zipWithIndex.traverse(validate.tupled).map(_ => ()))
 
