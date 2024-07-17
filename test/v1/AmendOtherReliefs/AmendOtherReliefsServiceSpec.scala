11884141a38485a140947c6564f4a46028fb350f
Michael Olafisoye <molafisoye@gmail.com>
2024-07-17 10:09:13 +0100

    MTDSA-25132 move other reliefs to tax year based definitions

---
+++ test/v1/AmendOtherReliefs/AmendOtherReliefsServiceSpec.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.AmendOtherReliefs
+
+import api.controllers.EndpointLogContext
+import api.models.domain.{Nino, TaxYear}
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import support.UnitSpec
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.AmendOtherReliefs.def1.model.request.Def1_AmendOtherReliefsRequestBody
+import v1.AmendOtherReliefs.model.request.Def1_AmendOtherReliefsRequestData
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class AmendOtherReliefsServiceSpec extends UnitSpec {
+
+  "service" when {
+    "service call successful" must {
+      "return mapped result" in new Test {
+        MockAmendOtherReliefsConnector
+          .amend(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        await(service.amend(requestData)) shouldBe Right(ResponseWrapper(correlationId, ()))
+      }
+    }
+  }
+
+  "unsuccessful" must {
+    "map errors according to spec" when {
+
+      def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
+        s"a $downstreamErrorCode error is returned from the service" in new Test {
+
+          MockAmendOtherReliefsConnector
+            .amend(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
+
+          await(service.amend(requestData)) shouldBe Left(ErrorWrapper(correlationId, error))
+        }
+
+      val errors = Seq(
+        "INVALID_TAXABLE_ENTITY_ID"        -> NinoFormatError,
+        "INVALID_TAX_YEAR"                 -> TaxYearFormatError,
+        "INVALID_CORRELATIONID"            -> InternalError,
+        "BUSINESS_VALIDATION_RULE_FAILURE" -> RuleSubmissionFailedError,
+        "SERVER_ERROR"                     -> InternalError,
+        "SERVICE_UNAVAILABLE"              -> InternalError
+      )
+
+      val extraTysErrors = Seq(
+        "INVALID_CORRELATION_ID" -> InternalError,
+        "INVALID_PAYLOAD"        -> InternalError,
+        "TAX_YEAR_NOT_SUPPORTED" -> RuleTaxYearNotSupportedError,
+        "UNPROCESSABLE_ENTITY"   -> InternalError
+      )
+
+      (errors ++ extraTysErrors).foreach(args => (serviceError _).tupled(args))
+    }
+  }
+
+  trait Test extends MockAmendOtherReliefsConnector {
+    implicit val hc: HeaderCarrier              = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new AmendOtherReliefsService(
+      connector = mockAmendOtherReliefsConnector
+    )
+
+    val taxYear: String                = "2017-18"
+    val nino: String                   = "AA123456A"
+    implicit val correlationId: String = "X-123"
+
+    val body: Def1_AmendOtherReliefsRequestBody = Def1_AmendOtherReliefsRequestBody(None, None, None, None, None, None, None)
+
+    protected val requestData: Def1_AmendOtherReliefsRequestData = Def1_AmendOtherReliefsRequestData(Nino(nino), TaxYear.fromMtd(taxYear), body)
+
+  }
+
+}
---
+++ test/v1/RetrieveOtherReliefs/RetrieveOtherReliefsServiceSpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.RetrieveOtherReliefs
+
+import api.controllers.EndpointLogContext
+import api.models.domain.{Nino, TaxYear, Timestamp}
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import support.UnitSpec
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.RetrieveOtherReliefs.def1.model.request.Def1_RetrieveOtherReliefsRequestData
+import v1.RetrieveOtherReliefs.def1.model.response._
+import v1.RetrieveOtherReliefs.model.response.Def1_RetrieveOtherReliefsResponse
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class RetrieveOtherReliefsServiceSpec extends UnitSpec {
+
+  private val nino: String           = "AA123456A"
+  private val taxYear: String        = "2017-18"
+  implicit val correlationId: String = "X-123"
+
+  private val fullResponseModel = Def1_RetrieveOtherReliefsResponse(
+    Timestamp("2020-06-17T10:53:38.000Z"),
+    Some(Def1_NonDeductibleLoanInterest(Some("myref"), 763.00)),
+    Some(Def1_PayrollGiving(Some("myref"), 154.00)),
+    Some(Def1_QualifyingDistributionRedemptionOfSharesAndSecurities(Some("myref"), 222.22)),
+    Some(Seq(Def1_MaintenancePayments(Some("myref"), Some("Hilda"), Some("2000-01-01"), 222.22))),
+    Some(
+      Seq(
+        Def1_PostCessationTradeReliefAndCertainOtherLosses(
+          Some("myref"),
+          Some("ACME Inc"),
+          Some("2019-08-10"),
+          Some("Widgets Manufacturer"),
+          Some("AB12412/A12"),
+          222.22))),
+    Some(Def1_AnnualPaymentsMade(Some("myref"), 763.00)),
+    Some(Seq(Def1_QualifyingLoanInterestPayments(Some("myref"), Some("Maurice"), 763.00)))
+  )
+
+  private val requestData = Def1_RetrieveOtherReliefsRequestData(Nino(nino), TaxYear.fromMtd(taxYear))
+
+  trait Test extends MockRetrieveOtherReliefsConnector {
+    implicit val hc: HeaderCarrier              = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new RetrieveOtherReliefsService(
+      connector = mockConnector
+    )
+
+  }
+
+  "service" should {
+    "return a successful response" when {
+      "a successful response is passed through" in new Test {
+        MockRetrieveOtherReliefsConnector
+          .retrieve(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, fullResponseModel))))
+
+        await(service.retrieve(requestData)) shouldBe Right(ResponseWrapper(correlationId, fullResponseModel))
+      }
+    }
+
+    "map errors according to spec" when {
+      def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
+        s"a $downstreamErrorCode error is returned from the service" in new Test {
+
+          MockRetrieveOtherReliefsConnector
+            .retrieve(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
+
+          await(service.retrieve(requestData)) shouldBe Left(ErrorWrapper(correlationId, error))
+        }
+
+      val errors = Seq(
+        ("INVALID_TAXABLE_ENTITY_ID", NinoFormatError),
+        ("INVALID_TAX_YEAR", TaxYearFormatError),
+        ("NO_DATA_FOUND", NotFoundError),
+        "INVALID_CORRELATIONID" -> InternalError,
+        ("SERVER_ERROR", InternalError),
+        ("SERVICE_UNAVAILABLE", InternalError)
+      )
+
+      val extraTysErrors = Seq(
+        "INVALID_CORRELATION_ID" -> InternalError,
+        "TAX_YEAR_NOT_SUPPORTED" -> RuleTaxYearNotSupportedError
+      )
+
+      (errors ++ extraTysErrors).foreach(args => (serviceError _).tupled(args))
+    }
+  }
+
+}
