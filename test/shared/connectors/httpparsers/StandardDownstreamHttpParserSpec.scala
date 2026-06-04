3d40f6f7717b4d133cc7237a06faca83c689144b
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2026-06-04 10:23:36 +0100

    POC to improve logging and diagnostics for successful downstream response JSON validation failures
    POC to improve logging and diagnostics for successful downstream response JSON validation failures

--- test/shared/connectors/httpparsers/StandardDownstreamHttpParserSpec.scala
+++ test/shared/connectors/httpparsers/StandardDownstreamHttpParserSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,48 +112,6 @@ class StandardDownstreamHttpParserSpec extends UnitSpec {
         val httpResponse = HttpResponse(PARTIAL_CONTENT, "", headers = Map("CorrelationId" -> List(correlationId)))
 
         httpReads.read(method, url, httpResponse) shouldBe Right(ResponseWrapper(correlationId, ()))
-      }
-    }
-  }
-
-  "validateJson" when {
-    implicit val reads: Reads[SomeDataObject] = Json.reads[SomeDataObject]
-
-    "the JSON is valid" should {
-      "return the parsed model" in {
-        val validJsonResponse: HttpResponse = HttpResponse(
-          OK,
-          expectedJson,
-          Map("CorrelationId" -> Seq(correlationId))
-        )
-
-        val result: Option[SomeDataObject] = validJsonResponse.validateJson[SomeDataObject]
-
-        result shouldBe Some(downstreamResponseData)
-      }
-    }
-
-    "the JSON is invalid" should {
-      "return None" in {
-        val invalidJsonResponse: HttpResponse = HttpResponse(
-          OK,
-          Json.obj("data"     -> 1234),
-          Map("CorrelationId" -> Seq(correlationId))
-        )
-
-        val result: Option[SomeDataObject] = invalidJsonResponse.validateJson[SomeDataObject]
-
-        result shouldBe None
-      }
-    }
-
-    "the response contains no JSON" should {
-      "return None" in {
-        val emptyResponse: HttpResponse = HttpResponse(OK, "", Map("CorrelationId" -> Seq(correlationId)))
-
-        val result: Option[SomeDataObject] = emptyResponse.validateJson[SomeDataObject]
-
-        result shouldBe None
       }
     }
   }
